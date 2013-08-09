46eed2fbae15166e2e00d01de7a989255f29a4e0
Rob Parrott <robparrott@gmail.com>
2013-08-09 14:13:17 -0400

    add in a basic unmabaged simple website, and a two-tier simple website example.

---
+++ deployments/scripts/simple-website.sh
@@ -0,0 +1,8 @@
+OUTFILE=/var/www/html/index.php
+cat <<'EOF' > ${OUTFILE}
+<?php
+echo '<h1>AWS CloudFormation sample PHP application</h1>';
+?>
+EOF
+chmod 644  ${OUTFILE}
+chown root:root ${OUTFILE}
