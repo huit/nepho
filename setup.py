5097b1fc25df62fbc5f3b7767d9e2aae2b2b2dc5
Daniel Silverman <me@agperson.com>
2013-08-15 21:00:19 -0400

    Expanded setup file and restructured data Distutils setup script suitable for distribution.  Renamed data files to fit best practice. TODO: update clidriver as per: http://peak.telecommunity.com/DevCenter/PkgResources#basic-resource-access TODO: Move YAML parsing out of clidriver and up a level.

--- setup.py
+++ setup.py
@@ -1,5 +1,32 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
-setup(name='nepho',
-      version='0.0.2',
-      packages=['nepho'],
-      )
+
+setup(
+    name         = 'nepho',
+    version      = '0.1.0',
+    url          = 'http://github.com/huit/nepho',
+    description  = 'Simplified cloud orchestration tool for constructing virtual data centers',
+    packages     = ['nepho', 'nepho.aws'],
+    author       = 'Harvard University Information Technology',
+    author_email = 'ithelp@harvard.edu',
+    license      = 'MIT',
+    scripts      = ['bin/nepho'],
+    package_data = {
+      'nepho': [
+        'data/deployments/*.yaml',
+        'data/deployments/scripts/*.sh',
+        ],
+      'nepho.aws': [
+        'data/patterns/*/*.cf'
+        'data/drivers/*.sh'
+        ]
+      },
+    requires     = [
+      'argparse (>=1.2)',
+      'boto (>=2.0)',
+      'awscli (>=0.13)',
+      'Jinja2',
+      'PyYAML'
+    ],
+  )
