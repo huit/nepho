12a0e71197bcb951d7709963a54e53c2aebfab93
Daniel Silverman <me@agperson.com>
2013-08-22 16:31:21 -0400

    removing plugin

--- nepho/plugin.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def serialize(subcmd,deployment,opts):
-    payload = dict()
-    payload['subcmd'] = subcmd
-    payload['deployment'] = deployment    
-    payload['opts'] = opts
-    return json.dumps(payload, sort_keys=True, indent=2,)
