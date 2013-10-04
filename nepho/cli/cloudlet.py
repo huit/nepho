a0e70535958a4453450ac6fda8d0bd95e3857bad
Daniel Silverman <me@agperson.com>
2013-10-04 15:03:33 -0400

    Adding argument for passing actual input

--- nepho/cli/cloudlet.py
+++ nepho/cli/cloudlet.py
@@ -8,7 +8,10 @@ class NephoCloudletController(base.NephoBaseController):
         label = 'cloudlet'
         stacked_on = None
         description = 'find, download, and manage cloudlets'
-        usage = "nepho cloudlet <action> [options]"
+        usage = "nepho cloudlet <action> <input>"
+        arguments = [
+            (['input'], dict(help='Input to subcommand, i.e. a search term, git URL, or cloudlet name', nargs='?')),
+        ]
 
     @controller.expose(help="List all installed cloudlets")
     def list(self):
@@ -16,16 +19,16 @@ class NephoCloudletController(base.NephoBaseController):
 
     @controller.expose(help="Search the Nepho Cloudlet Registry for cloudlets whose names, summaries, or descriptions match the provided search term")
     def search(self):
-        print "Unimplemented action"
+        print "Unimplemented action. (input: %s)" % self.pargs.input
 
     @controller.expose(help="Install a Nepho cloudlet from the Nepho Cloudlet Registry or from an external Git repository")
     def install(self):
-        print "Unimplemented action"
+        print "Unimplemented action. (input: %s)" % self.pargs.input
 
     @controller.expose(help="Upgrade an installed Nepho cloudlet")
     def upgrade(self):
-        print "Unimplemented action"
+        print "Unimplemented action. (input: %s)" % self.pargs.input
 
     @controller.expose(help="Uninstall a Nepho cloudlet")
     def uninstall(self):
-        print "Unimplemented action"
+        print "Unimplemented action. (input: %s)" % self.pargs.input
