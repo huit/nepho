57a3505b5d1fb82be1fde435cb230db21d467894
Rob Parrott <robparrott@gmail.com>
2013-11-04 17:57:00 -0500

    insert parameters into the context as a top level object.

--- nepho/core/context.py
+++ nepho/core/context.py
@@ -1,17 +1,14 @@
 # coding: utf-8
 import yaml
+import copy
 #from nepho.core import common
 from os import path
 
 class ContextManager:
     """
-        A configuration handler class for Nepho. 
+        A context handler class for Nepho. 
         
-        Currently wraps ConfigParser object, but could be reimplemented
-        as needed. Basically defines getters an setters.
-        
-        Saves the state on every set invocation.
-        
+        Produces a context dictionary for use with templating engines.
     """
     
     def __init__(self, cfgMgr):
@@ -31,81 +28,21 @@ class ContextManager:
         """Generates a context object to be injected into the templating engine."""
         
         context = dict()
-        context['config'] = self.configManager.to_dict()
+        config = self.configManager.to_dict()
+        if "parameters" in config:
+            context['parameters'] = copy.copy(config['parameters'])
+                    
+        context['config'] = config
         
         if self.blueprint is not None:
             cloudlt = self.blueprint.cloudlet
             context['cloudlet'] = cloudlt.defn
-            context['blueprint '] = self.blueprint.defn
-                            
-#         context = pattern.get_context()
-#         context['config'] = self.config.to_dict()
-#         try:
-#             context[providr.PROVIDER_ID] = self.config.to_dict(providr.PROVIDER_ID)
-#         except:
-#             pass 
-#         
-#         try:
-#             context['parameters'] = self.config.to_dict("parameters")
-#         except:
-#             pass 
-#         #context['cloudlet'] 
-        
-        return context
-    
-        
-# 
-# 
-#         
-#         
-#     
-#             
-#     def keys(self, domain="nepho"):
-#         """Return a set of config names"""
-#         return self.ini_config.keys(domain)
-#         
-#        
-#     def load(self, config_file):
-#         """Load in configs from local settings YAML file."""
-#         try:
-#             cnfg = yaml.load(open(config_file))
-#             for k in cnfg.keys():
-#                 self.set(k, cnfg[k])
-#         except Exception as e:
-#             pass
-#     
-#     def save(self):
-#         """Saves out configs to the local config file."""
-#         d = self.to_dict()
-#         config_file = self.get("local_config")
-#         
-#         try:
-#             cnfg = yaml.dump(d, open(config_file, "w"))
-#         except Exception as e:
-#                 print "Error writing nepho local config YAML file!"
-#                 print e
-#                 exit(1)      
-#                 
-#                 
-#     def get(self, key, domain="nepho"):
-#         """Basic getter for config values."""
-#         if domain is None:
-#             domain = "nepho"
-#         return self.ini_config.get(domain, key)
-#                         
-#                         
-#     def set(self, key, value, domain="nepho"):  
-#         """Basic setter for config values."""
-#         if domain is None:
-#             domain = "nepho"
-#         self.ini_config.set(domain, key, value)
-#         self.save()
-#             
-#             
-#     def to_dict(self, domain="nepho"):
-#         """Converts the configs to a dictionary for convenience"""
-#         d = dict()
-#         for k in self.keys():
-#             d[k] = self.get(k)
-#         return d
+            context['blueprint'] = self.blueprint.defn
 
+        #
+        # Temporary measure
+        #
+        context['scripts'] = None
+
+        return context
+  
