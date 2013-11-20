00ea212a4e62700b984d36b298fb5cc9a2e966e5
Tim Hartmann <tfhartmann@gmail.com>
2013-11-20 00:04:44 -0500

    Updated Nepho to bootstrap hooks and handlers
    Updated Nepho cli code to use the bootrap method to initialize hooks
    and handlers. This is related to issue #88. For reference look at
    the comment in the issue and this link
    http://cement.readthedocs.org/en/latest/api/core/foundation/#cement.core.foundation.CementApp.Meta.bootstrap

--- tests/nepho_test.py
+++ tests/nepho_test.py
@@ -3,17 +3,71 @@ from cement.utils import test
 from nepho import cli
 from nepho.cli.base import Nepho
 
-
-class MyTestApp(Nepho):
+class NephoTestApp(Nepho):
     class Meta:
         argv = []
         config_files = []
 
 
-class MyTestCase(test.CementTestCase):
-    app_class = MyTestApp
+class BasicNephoTests(test.CementTestCase):
+    app_class = NephoTestApp
 
     def test_nepho_default(self):
         self.app.setup()
         self.app.run()
         self.app.close()
+
+
+# Test Cloudlet
+class TestNephoCloudlet(test.CementTestCase):
+    app_class = NephoTestApp
+
+    def test_nepho_cloudlet_install(self):
+        app = self.make_app(argv=['cloudlet', 'search' ])
+        app.setup()
+        app.run()
+        app.close()
+
+    def test_nepho_cloudlet_search(self):
+        app = self.make_app(argv=['cloudlet', 'search' ])
+        app.setup()
+        app.run()
+        app.close()
+
+
+# Test Blueprint
+class TestNephoBlueprint(test.CementTestCase):
+    app_class = NephoTestApp
+
+    def test_nepho_blueprint_list(self):
+        app = self.make_app(argv=['blueprint', 'list', 'theforeman' ])
+        app.setup()
+        app.run()
+        app.close()
+
+
+# Test Config
+class TestNephoConfig(test.CementTestCase):
+    app_class = NephoTestApp
+
+    def test_nepho_config_list(self):
+        app = self.make_app(argv=['config', 'list' ])
+        app.setup()
+        app.run()
+        app.close()
+
+
+# Test Parameter
+class TestNephoParameter(test.CementTestCase):
+    app_class = NephoTestApp
+
+
+
+# Test Scope
+class TestNephoScope(test.CementTestCase):
+    app_class = NephoTestApp
+
+
+# Test Stack
+class TestNephoStack(test.CementTestCase):
+    app_class = NephoTestApp
