f471cfda7b9ccd0960231b6210b94599bff7a78c
Daniel Silverman <me@agperson.com>
2014-01-30 16:36:07 -0500

    Name parameter is not AWS-only anymore

--- nepho/cli/stack.py
+++ nepho/cli/stack.py
@@ -26,7 +26,7 @@ class NephoStackController(base.NephoBaseController):
             (['cloudlet'], dict(help=argparse.SUPPRESS, nargs='?')),
             (['blueprint'], dict(help=argparse.SUPPRESS, nargs='?')),
             (['--save', '-s'], dict(help=argparse.SUPPRESS, action='store_true')),
-            (['--name', '-n'], dict(help='custom stack name (aws only)', default=None)),
+            (['--name', '-n'], dict(help='custom stack name', default=None)),
             (['--develop', '-d'], dict(help='development mode (vagrant only)', action='store_true')),
             (['--params', '-p'], dict(help=argparse.SUPPRESS, nargs='*', action='append')),
         ]
